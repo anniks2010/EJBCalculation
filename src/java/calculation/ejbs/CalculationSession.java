d70f4d1618169d314e78e35888433fc6a30c36cf
Anu Lember <anulember@hot.ee>
2021-02-10 11:26:02 +0200

    JSF

---
+++ src/java/calculation/ejbs/CalculationSession.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation.ejbs;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author opilane
+ */
+@Stateless
+public class CalculationSession implements CalculationSessionLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
